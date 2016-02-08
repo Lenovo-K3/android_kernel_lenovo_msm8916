48f4f3ded26a87d3e4c9b03e5354c6fd996572ec
Mansi <slfl@mail.ru>
2016-02-08 19:07:00 +0300

    build: update script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# By Mansi
 
 OUT_DIR="out"
 KERNEL_DIR=$PWD
@@ -14,7 +15,6 @@ nocol='\033[0m'
 
 # Set configs
 mkdir ${OUT_DIR}
-mkdir ${KERNEL_DIR}/Mansi/modules
 export CROSS_COMPILE=~/toolchains/arm-eabi-4.8/bin/arm-eabi-
 export JAVA_HOME=/usr/lib/jvm/java-7-oracle
 export USE_CCACHE=1
@@ -55,27 +55,6 @@ esac
 
 echo -e "$cyan Build dtb file $nocol"
 scripts/dtbToolCM -2 -o ${OUT_DIR}/arch/arm/boot/dt.img -s 2048 -p ${OUT_DIR}/scripts/dtc/ ${OUT_DIR}/arch/arm/boot/dts/
-
-echo -e "$cyan Copy modules $nocol"
-cp ${OUT_DIR}/arch/arm/mach-msm/dma_test.ko					${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/arch/arm/mach-msm/msm-buspm-dev.ko 			${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/arch/arm/oprofile/oprofile.ko 				${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/block/test-iosched.ko 						${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/crypto/ansi_cprng.ko 							${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/drivers/crypto/msm/qcrypto.ko 				${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/drivers/gud/mcDrvModule.ko 					${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/drivers/gud/mcKernelApi.ko 					${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/drivers/input/evbug.ko 						${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/drivers/input/misc/gpio_axis.ko 				${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/drivers/input/misc/gpio_event.ko 				${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/drivers/input/misc/gpio_input.ko 				${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/drivers/input/misc/gpio_matrix.ko 			${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/drivers/input/misc/gpio_output.ko 			${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/drivers/media/radio/radio-iris-transport.ko 	${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/drivers/mmc/card/mmc_block_test.ko 			${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/drivers/mmc/card/mmc_test.ko 					${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/drivers/spi/spidev.ko 						${KERNEL_DIR}/Mansi/modules/
-cp ${OUT_DIR}/drivers/usb/host/ice40-hcd.ko 				${KERNEL_DIR}/Mansi/modules/
 
 echo -e "$cyan Copy kernel $nocol"
 cp ${OUT_DIR}/arch/arm/boot/dt.img  ${KERNEL_DIR}/Mansi/Output/dt.img
